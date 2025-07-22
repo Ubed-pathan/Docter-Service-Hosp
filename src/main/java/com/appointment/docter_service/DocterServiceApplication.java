4f465dc326adbbe7d0be0f209b00cabeb5f8c5e2
Ubed-pathan <ubedpathan818@gmail.com>
2025-07-22 15:05:59 +0530

    added eureka client

--- src/main/java/com/appointment/docter_service/DocterServiceApplication.java
+++ src/main/java/com/appointment/docter_service/DocterServiceApplication.java
@@ -2,8 +2,11 @@ package com.appointment.docter_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient // this is for eureka Enable service discovery for this application
 public class DocterServiceApplication {
 
 	public static void main(String[] args) {
